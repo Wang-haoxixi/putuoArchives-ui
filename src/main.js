1085efe6e64cbab7545ee067d1e5f91e29d2b5bc
passerr <passerr@163.com>
2021-11-15 10:15:31 +0800

    主题调整

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import Cookies from 'js-cookie'
-
 import Element from 'element-ui'
 // import './assets/styles/element-variables.scss'
 
-import '../theme/index.css' // theme
+import '@/assets/styles/theme/index.css' // theme
 import '@/assets/styles/index.scss' // global css
 import '@/assets/styles/ruoyi.scss' // ruoyi css
 import App from './App'

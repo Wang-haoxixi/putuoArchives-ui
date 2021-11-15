6503fd40a49436156a2c4d7386ba94a33af0fa12
passerr <passerr@163.com>
2021-11-15 11:53:04 +0800

    首页背景替换 按钮样式修改

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,76 +1,29 @@
 <template>
-  <div class="dashboard-editor-container">
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="success">测试</el-button>
-    <panel-group @handleSetLineChartData="handleSetLineChartData" />
-
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData" />
-    </el-row>
-
-    <el-row :gutter="32">
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart />
-        </div>
-      </el-col>
-    </el-row>
-
-    
+  <div>我的任务
+    <div>
+      <el-button type="primary" :plain="curret !=0" @click="click(0)">进行中任务</el-button>
+      <el-button type="primary" :plain="curret !=1" @click="click(1)">待领取任务</el-button>
+      <el-button type="primary" :plain="curret !=2" @click="click(2)">完善任务</el-button>
+      <el-button type="primary" :plain="curret !=3" @click="click(3)">全部任务</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-import PanelGroup from './dashboard/PanelGroup'
-import LineChart from './dashboard/LineChart'
-import RaddarChart from './dashboard/RaddarChart'
-import PieChart from './dashboard/PieChart'
-import BarChart from './dashboard/BarChart'
-
-const lineChartData = {
-  newVisitis: {
-    expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
-  },
-  messages: {
-    expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
-  },
-  purchases: {
-    expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
-  },
-  shoppings: {
-    expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
-
 export default {
   name: 'Index',
   components: {
-    PanelGroup,
-    LineChart,
-    RaddarChart,
-    PieChart,
-    BarChart
+    
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
+      curret: 0,
     }
   },
   methods: {
+    click(index){
+      this.curret = index;
+    },
     handleSetLineChartData(type) {
       this.lineChartData = lineChartData[type]
     }
@@ -80,7 +33,7 @@ export default {
 
 <style lang="scss" scoped>
 .dashboard-editor-container {
-  padding: 32px;
+  // padding: 32px;
   background-color: rgb(240, 242, 245);
   position: relative;
 

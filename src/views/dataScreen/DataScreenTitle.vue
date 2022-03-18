ed5c050ba5954723ad05b80c24a05ece2c527de5
passerr <passerr@163.com>
2022-03-18 11:39:00 +0800

    新版数据驾驶舱页面构建

---
+++ src/views/dataScreen/DataScreenTitle.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="title">
+    <div class="title-container">
+      <img
+        class="title-border"
+        src="@/assets/images/bg_dataScreen_border_left@2x.png"
+      />
+      <div>
+        <div class="title-bg-gradient">
+          <img
+            class="title-bg"
+            src="@/assets/images/bg_dataScreen_title_left@2x.png"
+          />
+          {{ title }}
+          <img
+            class="title-bg"
+            src="@/assets/images/bg_dataScreen_title_right@2x.png"
+          />
+        </div>
+        <div class="title-bottom"></div>
+      </div>
+      <img
+        class="title-border"
+        src="@/assets/images/bg_dataScreen_border_right@2x.png"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  // margin: 0 44px;
+  text-align: center;
+  font-weight: 500;
+  line-height: 25px;
+  color: #3ce7fd;
+  .title-container {
+    justify-content: center;
+    display: flex;
+    align-items: flex-end;
+    .title-border {
+      width: 80px;
+      height: 23px;
+    }
+    .title-bg {
+      margin: 0 8px;
+      width: 26px;
+      height: 9px;
+    }
+  }
+}
+.title-bottom {
+  height: 1px;
+  background: linear-gradient(270deg, #0071e2 0%, #ffffff 50%, #0071e2 100%);
+}
+.title-bg-gradient {
+  padding: 9px 0;
+  background: linear-gradient(
+    270deg,
+    rgba(9, 75, 168, 0) 0%,
+    #094ba8 50%,
+    rgba(9, 75, 168, 0) 100%
+  );
+}
+</style>
---
+++ src/views/dataScreen/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <basic-container>
+    <el-row :gutter="20" type="flex" style="flex: 1.3">
+      <el-col :span="10" style="height: 100%"
+        ><div class="content">
+          <data-screen-title title="档案室情况"></data-screen-title>
+        </div>
+      </el-col>
+      <el-col :span="14" style="height: 100%"
+        ><div class="content">
+          <data-screen-title title="队伍建设"></data-screen-title></div
+      ></el-col>
+    </el-row>
+    <el-row :gutter="20" style="padding-top: 20px; flex: 1">
+      <el-col :span="10" style="height: 100%"
+        ><div class="content">
+          <data-screen-title title="归集任务完成情况"></data-screen-title>
+        </div>
+      </el-col>
+      <el-col :span="8" style="height: 100%"
+        ><div class="content">
+          <data-screen-title title="任务完成情况"></data-screen-title></div
+      ></el-col>
+      <el-col :span="6" style="height: 100%"
+        ><div class="content">
+          <data-screen-title title="OA在线归档情况"></data-screen-title></div
+      ></el-col>
+    </el-row>
+  </basic-container>
+</template>
+
+<script >
+import DataScreenTitle from "./DataScreenTitle";
+export default {
+  components: { DataScreenTitle },
+};
+</script>
+
+<style lang="scss" scoped>
+.basic-container {
+  background-color: #0b1667;
+  height: calc(100vh - 144px);
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  height: 100%;
+  border: 1px solid rgba(0, 74, 193, 0.9882352941176471);
+  color: white;
+  box-shadow: 0 0 31px #037de673 inset;
+  background-image: url("../../assets/images/icon_dataScreen_1.png"),
+    url("../../assets/images/icon_dataScreen_2.png"),
+    url("../../assets/images/icon_dataScreen_3.png"),
+    url("../../assets/images/icon_dataScreen_4.png");
+  background-repeat: no-repeat, no-repeat, no-repeat, no-repeat;
+  background-position: top left, top right, bottom left, bottom right;
+  background-color: linear-gradient(
+    359deg,
+    rgba(0, 100, 186, 0.25) 0%,
+    rgba(3, 125, 230, 0.06) 100%
+  );
+  .title {
+    // margin: 0 44px;
+    text-align: center;
+    font-weight: 500;
+    line-height: 25px;
+    color: #3ce7fd;
+    .title-container {
+      justify-content: center;
+      display: flex;
+      align-items: flex-end;
+      .title-border {
+        width: 80px;
+        height: 23px;
+      }
+      .title-bg {
+        margin: 0 8px;
+        width: 26px;
+        height: 9px;
+      }
+    }
+  }
+  .title-bottom {
+    height: 1px;
+    background: linear-gradient(270deg, #0071e2 0%, #ffffff 50%, #0071e2 100%);
+  }
+}
+</style>
